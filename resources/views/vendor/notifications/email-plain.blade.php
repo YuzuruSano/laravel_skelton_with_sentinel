cd7e838e15dbece1412e9a6603c44a39eb8a6c4e
Yuzuru Sano(mac book) <sano@suitosha.co.jp>
2018-02-22 21:06:46 +0900

    email  認証用のhtmlメールテンプレート

---
+++ resources/views/vendor/notifications/email-plain.blade.php
@@ -0,0 +1,17 @@
+<?php
+if (! empty($greeting)) {
+    echo $greeting, "\n\n";
+} else {
+    echo $level == 'error' ? 'Error!' : 'Hello!', "\n\n";
+}
+if (! empty($introLines)) {
+    echo implode("\n", $introLines), "\n\n";
+}
+if (isset($actionText)) {
+    echo "{$actionText}", "\n", "{$actionUrl}", "\n\n";
+}
+if (! empty($outroLines)) {
+    echo implode("\n", $outroLines), "\n\n";
+}
+echo trans('sentinel.notify_footer_message'), "\n";
+echo config('app.name'), trans('sentinel.system_mail'), "\n";

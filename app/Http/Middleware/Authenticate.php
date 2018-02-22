7c4204ee17c841c2aa1319cd226a7d0e08732c8a
Yuzuru Sano(mac book) <sano@suitosha.co.jp>
2018-02-22 21:05:22 +0900

    sentinel用にmiddlewareを変更

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Sentinel;
+
+/**
+ * Handle an incoming request.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  \Closure  $next
+ * @return mixed
+ */
+public function handle($request, Closure $next)
+{
+    if (Sentinel::guest()) {
+        if ($request->ajax() || $request->wantsJson()) {
+            // ajaxでのアクセスや、JSONの場合はテキストのみ
+            return response('Unauthorized.', 401);
+        } else {
+            // ログインしていないのでログイン画面へ
+            return redirect()->guest('login');
+        }
+    }
+    // 認証しているので指定のルートへ
+    return $next($request);
+}
